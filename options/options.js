165ea2cd4146ff697061223b3f420a85723a06a3
Crowdbotics <team@crowdbotics.com>
2024-01-29 05:06:39 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "emory-careconnect-45655",
+  url: "https://emory-careconnect-45655.botics.co",
+  api: "https://emory-careconnect-45655.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
